8e3a4b41590ca2343b587ae7c89666b0e643247a
Leonardo Oliveira <lolive4@sibsnet.local>
2021-11-12 11:55:08 +0000

    Intent Working and New activity added with back arrow

--- app/src/main/java/com/example/galleryapp/MainActivity.kt
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.galleryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Intent working
+        text_view_hello.setOnClickListener{
+            val detailActivity = Intent(this, DetailsActivity::class.java)
+            startActivity(detailActivity)
+        }
     }
 }
